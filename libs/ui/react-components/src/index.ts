1b461f4d891f6bc572ad673dfe859643215e37d0
Labik Martin <mlab@ciklum.com>
2024-07-01 16:23:48 +0200

    verbatim module syntax imports & adding color mode & global styles cleanup

--- libs/ui/react-components/src/index.ts
+++ libs/ui/react-components/src/index.ts
@@ -1,1 +1,2 @@
 export { ReactComponents } from './lib/react-components';
+export { ToggleColorModeButton } from './lib/ToggleColorModeButton/ToggleColorModeButton';
--- libs/ui/react-components/src/lib/react-components.tsx
+++ libs/ui/react-components/src/lib/react-components.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { ToggleColorModeButton } from '../';
+
 import styles from './react-components.module.scss';
 
 /* eslint-disable-next-line */
@@ -8,6 +10,7 @@ export interface ReactComponentsProps {}
 export function ReactComponents(props: ReactComponentsProps) {
   return (
     <div className={styles['container']}>
+      <ToggleColorModeButton />
       <h1>Welcome to ReactComponents!</h1>
     </div>
   );
